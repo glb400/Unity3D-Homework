e14e3e34c32aa1f8d9a557bf25eb9d305d323b89
glb400 <glb400@126.com>
2019-12-24 18:33:07 +0800

    unityhw11

---
+++ hw11/ARtrain/Assets/Resources/vb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject btn;
+    public Animator anime;
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vbb) {
+        anime.SetTrigger("Jump");
+        Debug.Log("Jump");
+    }
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vbb){
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = btn.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
